e525a523b7951febe30cad36386b15105ce2c92c
Aleksey Minigaleev <Aleksey.Minigaleev@gmail.com>
2024-05-20 17:35:26 +0500

    #105 created new AuthenticationModel

---
+++ C4S.DB/Models/UserAuthenticationModel.cs
@@ -0,0 +1,97 @@
+﻿using Konscious.Security.Cryptography;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace C4S.DB.Models
+{
+    /// <summary>
+    /// Таблица авторизационных данных пользователя
+    /// </summary>
+    public class UserAuthenticationModel
+    {
+        /// <summary>
+        /// PK
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// FK
+        /// </summary>
+        public Guid UserId { get; set; }
+
+        public UserModel User { get; set; }
+
+        /// <summary>
+        /// Хэш пароля
+        /// </summary>
+        public byte[] PasswordHash { get; set; }
+
+        /// <summary>
+        /// Соль пароля
+        /// </summary>
+        public byte[] PasswordSalt { get; set; }
+
+        /// <summary>
+        /// Токен обновления
+        /// </summary>
+        public string? RefreshToken { get; private set; }
+
+        public UserAuthenticationModel(
+            UserModel user,
+            string password,
+            string? refreshToken = default)
+        {
+            Id = Guid.NewGuid();
+            User = user;
+            UserId = user.Id;
+            SetPassword(password);
+            RefreshToken = refreshToken;
+        }
+
+        private UserAuthenticationModel()
+        { }
+
+        /// <summary>
+        /// Устанавливает значения для полей <see cref="PasswordHash"/> и <see cref="PasswordSalt"/>
+        /// </summary>
+        /// <param name="password">Пароль пользователя</param>
+        public void SetPassword(string password)
+        {
+            PasswordSalt = RandomNumberGenerator.GetBytes(16);
+
+            /*TODO: по хорошему надо вынести все настройки argon2 в appsettings*/
+            var argon2 = new Argon2id(Encoding.UTF8.GetBytes(password))
+            {
+                Salt = PasswordSalt,
+                DegreeOfParallelism = 8,
+                MemorySize = 1024 * 1024,
+                Iterations = 4
+            };
+            PasswordHash = argon2.GetBytes(16);
+        }
+
+        /// <summary>
+        /// Проверяет введенный пароль на соответствие хэшу пароля пользователя.
+        /// </summary>
+        /// <param name="password">Пароль, который необходимо проверить.</param>
+        /// <returns> <see langword="true"/>, если введенный пароль соответствует хэшу пароля пользователя, иначе <see langword="false"/>.</returns>
+        public bool ValidatePassword(string password)
+        {
+            var argon2 = new Argon2id(Encoding.UTF8.GetBytes(password))
+            {
+                Salt = PasswordSalt,
+                DegreeOfParallelism = 8,
+                MemorySize = 1024 * 1024,
+                Iterations = 4
+            };
+            var hashToVerify = argon2.GetBytes(16);
+            return PasswordHash.SequenceEqual(hashToVerify);
+        }
+
+        /// <summary>
+        /// Устанавливает токен обновления
+        /// </summary>
+        public void SetRefreshToken(string? token) =>
+            RefreshToken = token;
+    }
+}
