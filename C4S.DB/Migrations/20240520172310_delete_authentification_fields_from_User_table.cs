9cbdee7b63ed196a0b7d676b7d7c8a46c496fe5f
Aleksey Minigaleev <Aleksey.Minigaleev@gmail.com>
2024-05-20 23:55:30 +0500

    #105 migratino with password encryption and delete password from User table

--- C4S.DB/Migrations/20240520172310_delete_authentification_fields_from_User_table.cs
+++ C4S.DB/Migrations/20240520172310_delete_authentification_fields_from_User_table.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using C4S.DB.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 #nullable disable
 
@@ -10,6 +15,43 @@ namespace C4S.DB.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile($"appsettings.{environmentName}.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            var serviceProvider = new ServiceCollection()
+                .AddDbContext<ReportDbContext>(options =>
+                    options.UseSqlServer(
+                        configuration.GetConnectionString("ReportDB")))
+                .BuildServiceProvider();
+
+            using var dbContext = serviceProvider.GetRequiredService<ReportDbContext>();
+
+            var connectionString = dbContext.Database.GetConnectionString();
+
+
+            using var connection = new SqlConnection(connectionString);
+            var query = "SELECT Password, Id FROM [UserAuthentication]";
+            var command = new SqlCommand(query, connection);
+
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var password = reader["Password"].ToString();
+                var id = Guid.Parse(reader["Id"].ToString());
+
+                var userAuthenticationModel = dbContext.UserAuthenticationModels
+                    .Single(x => x.Id == id);
+                userAuthenticationModel.SetPassword(password);
+            }
+            reader.Close();
+            dbContext.SaveChanges();
+
+
             migrationBuilder.DropColumn(
                 name: "Password",
                 table: "User");
