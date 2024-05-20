7701be67bc24f2aac5549fb6fc19c1b32baa1116
Aleksey Minigaleev <Aleksey.Minigaleev@gmail.com>
2024-05-20 22:24:41 +0500

    #105 migration

---
+++ C4S.DB/Migrations/20240520172310_delete_authentification_fields_from_User_table.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace C4S.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class delete_authentification_fields_from_User_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "User");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
