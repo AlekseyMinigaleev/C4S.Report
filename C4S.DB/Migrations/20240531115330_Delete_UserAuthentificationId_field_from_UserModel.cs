75ee04da1018c593c0f26ebafcaa520842ef1624
Aleksey Minigaleev <Aleksey.Minigaleev@gmail.com>
2024-05-31 17:03:56 +0500

    MIGRATION. Delete AuthentificationModelId field from UserModel

---
+++ C4S.DB/Migrations/20240531115330_Delete_UserAuthentificationId_field_from_UserModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace C4S.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class Delete_UserAuthentificationId_field_from_UserModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AuthenticationId",
+                table: "User");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "AuthenticationId",
+                table: "User",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+    }
+}
