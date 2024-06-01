b7730e2763589295e86b721d95df5385986b45f5
Aleksey Minigaleev <Aleksey.Minigaleev@gmail.com>
2024-06-01 20:48:03 +0500

    #143 MIGRATION rename isConfirmed to isActive

---
+++ C4S.DB/Migrations/20240601154048_rename_isConfirmEmail_field_to_IsActive.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace C4S.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class rename_isConfirmEmail_field_to_IsActive : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsEmailConfirmed",
+                table: "User",
+                newName: "IsActive");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "User",
+                newName: "IsEmailConfirmed");
+        }
+    }
+}
