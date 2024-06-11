9cfdb7e6a8a3617a343fa4ec90633df37d0e3221
Aleksey Minigaleev <Aleksey.Minigaleev@gmail.com>
2024-06-12 01:27:09 +0500

    #146 reset password action

---
+++ C4S.API/Features/UserAuthentication/UserAuthentificationController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C4S.API.Features.UserAuthentication
+{
+    public class UserAuthentificationController : BaseApiController
+    {
+        public UserAuthentificationController(IMediator mediator) : base(mediator)
+        { }
+
+        [HttpPost("reset-password")]
+        public async Task<ActionResult> ResetPasswordAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
