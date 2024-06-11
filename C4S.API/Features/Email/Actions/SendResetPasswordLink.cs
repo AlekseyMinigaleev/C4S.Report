369623ff3cddcc6dc8b4f37b5aafed048b8fa04c
Aleksey Minigaleev <Aleksey.Minigaleev@gmail.com>
2024-06-12 00:46:24 +0500

    #146 add send reset password endpoint

---
+++ C4S.API/Features/Email/Actions/SendResetPasswordLink.cs
@@ -0,0 +1,81 @@
+﻿using C4S.DB;
+using C4S.Services.Services.EmailSenderService;
+using C4S.Services.Services.TokenService;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace C4S.API.Features.Email.Actions
+{
+    public class SendResetPasswordLink
+    {
+        public class Query : IRequest
+        {
+            public string Email { get; set; }
+        }
+
+        public class Validator : AbstractValidator<Query>
+        {
+            public Validator(ReportDbContext dbContext)
+            {
+                RuleFor(x => x.Email)
+                    .EmailAddress();
+
+                RuleFor(x => x.Email)
+                    .MustAsync(async (email, cancellationToken) =>
+                    {
+                        var user = await dbContext.Users
+                            .SingleOrDefaultAsync(
+                                x => x.Email == email && x.IsActive,
+                                cancellationToken);
+
+                        return user is not null;
+                    })
+                    .WithMessage("Пользователь с указанной почтой не был найден");
+            }
+        }
+
+        public class Handler : IRequestHandler<Query>
+        {
+            private readonly ReportDbContext _dbContext;
+            private readonly ITokenService _tokenService;
+            private readonly IEmailSenderService _emailSernderService;
+            private readonly string _frontBaseUrl;
+
+            public Handler(
+                ReportDbContext dbContext,
+                ITokenService tokenService,
+                IEmailSenderService emailService,
+                IConfiguration configuration)
+            {
+                _dbContext = dbContext;
+                _tokenService = tokenService;
+                _emailSernderService = emailService;
+
+                var frontBaseUrl = configuration["FrontBaseUrl"]!;
+                if (string.IsNullOrWhiteSpace(frontBaseUrl))
+                    throw new ArgumentNullException(
+                        $"{nameof(frontBaseUrl)}.В конфигурации приложения не указан FrontBaseUrl");
+                _frontBaseUrl = frontBaseUrl;
+            }
+
+            public async Task Handle(Query request, CancellationToken cancellationToken)
+            {
+                var user = await _dbContext.Users
+                    .SingleAsync(
+                        x => x.Email == request.Email && x.IsActive,
+                        cancellationToken);
+
+                var token = _tokenService.GenerateToken(user.Id);
+
+                var link = $"{_frontBaseUrl}/reset-password?token={token}";
+
+                await _emailSernderService.SendResetPasswordLink(
+                    emailTo: user.Email,
+                    subject: "Восстановление пароля",
+                    resetPasswordLink: link,
+                    cancellationToken);
+            }
+        }
+    }
+}
--- C4S.API/Features/Email/EmailController.cs
+++ C4S.API/Features/Email/EmailController.cs
@@ -11,8 +11,13 @@ namespace C4S.API.Features.Email
         public EmailController(IMediator mediator) : base(mediator)
         { }
 
+        /// <summary>
+        /// Отправляет код подтверждения на электронную почту пользователя.
+        /// </summary>
+        /// <param name="validator">Валидатор.</param>
+        /// <param name="cancellationToken">Токен отмены операции.</param>
         [Authorize]
-        [HttpPost("SendEmailVerificationCode")]
+        [HttpPost("send-email-verification-code")]
         public async Task<ActionResult> SendEmailVerificationCode(
            [FromServices] IValidator<SendEmailVerificationCode.Command> validator,
            CancellationToken cancellationToken)
@@ -28,8 +33,14 @@ namespace C4S.API.Features.Email
             return Ok();
         }
 
+        /// <summary>
+        /// Проверяет код подтверждения, отправленный на электронную почту пользователя.
+        /// </summary>
+        /// <param name="query">Запрос на проверку кода подтверждения.</param>
+        /// <param name="validator">Валидатор.</param>
+        /// <param name="cancellationToken">Токен отмены операции.</param>
         [Authorize]
-        [HttpPost("VerifyEmailVerificationCode")]
+        [HttpPost("verify-email-verification-code")]
         public async Task<ActionResult> VerifyEmailVerificationCode(
             [FromBody] VerifyEmailVerificationCode.Query query,
             [FromServices] IValidator<VerifyEmailVerificationCode.Query> validator,
@@ -43,6 +54,28 @@ namespace C4S.API.Features.Email
             var result = await Mediator.Send(query, cancellationToken);
 
             return Ok(result);
+        }
+
+        /// <summary>
+        /// Отправляет ссылку для сброса пароля на указанную электронную почту.
+        /// </summary>
+        /// <param name="query">Запрос на отправку ссылки для сброса пароля.</param>
+        /// <param name="validator">Валидатор.</param>
+        /// <param name="cancellationToken">Токен отмены операции.</param>
+        [HttpPost("send-reset-password-link")]
+        public async Task<ActionResult> SendResetPasswordLink(
+            [FromBody] SendResetPasswordLink.Query query,
+            [FromServices] IValidator<SendResetPasswordLink.Query> validator,
+            CancellationToken cancellationToken)
+        {
+            await ValidateAndChangeModelStateAsync(validator, query, cancellationToken);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await Mediator.Send(query, cancellationToken);
+
+            return Ok();
         }
     }
 }
